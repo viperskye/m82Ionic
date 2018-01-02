976c7d953ffa73296d1026cec7b38be782d7db56
viperskye <viperskye@hotmail.com>
2018-01-02 16:03:53 +0700

    New data

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
 import { LoadingController } from 'ionic-angular';
 import { ModalController, NavParams, ViewController } from 'ionic-angular';
+import { Content } from 'ionic-angular';
 import { NavController } from 'ionic-angular';
 import { ToastController } from 'ionic-angular';
 import { Http } from '@angular/http';
@@ -10,21 +11,26 @@ import { Http } from '@angular/http';
   templateUrl: 'home.html'
 })
 export class HomePage {
+	@ViewChild(Content) content: Content;
 	selectTab: string = 'tc';
-	kiemtra: boolean = true;
-	page: boolean = true;
+	kiemtra: boolean = false;
+	page: boolean = false;
+	check:boolean = true;
 	Textm82: string;
+	dem: number= 0;
 	thongbao: string = 'Code by Viperskye';
-	demnhom: Number;
+	demnhom: Number = 0;
 	db: any = null;
-	mang: any = [];
-	obj: any;
+	search:string;
+	mang: any = [0];
+	obj: any ={"ObjDe": [],"ObjEn": []};
 	constructor(public navCtrl: NavController,
 				public loadingCtrl: LoadingController, 
 				public http: Http, 
 				public modalCtrl: ModalController,
 				private toastCtrl: ToastController) {
 		console.log('ở đây trước');
+		this.page = true;
 	}
 	ionViewDidLoad() {
 		console.log('ở đây sau');
@@ -44,10 +50,46 @@ export class HomePage {
 			this.createToast('Lỗi: Không khởi tạo được Storage');
 		}
 	}
+	public  hello():any {
+		if(this.check == false) {
+			if(this.dem == 0) {
+				this.dem = +this.dem+1;
+				this.check = true;
+				return false;
+			} else {
+				this.dem = 0;
+				this.check = true;
+				this.hello();
+			}
+		}
+		this.obj.ObjEn[this.demnhom.toString()] = this.search;
+		var tmp = this.find(this.db, this.search,'so');
+		this.obj.ObjDe[this.demnhom.toString()] = tmp['chu'];
+		if(this.search.length == 5) {
+			if(tmp['chu'] != 'Error') {
+				this.demnhom = +this.demnhom+1;
+				if (this.demnhom < this.obj.ObjEn.length) {
+					this.check = false;
+					this.dem = 0;
+					this.search = this.obj.ObjEn[this.demnhom.toString()];
+				} else {
+					this.demnhom = this.obj.ObjEn.length;
+					this.search = '';
+				}
+			}
+			if(+this.demnhom%4 == 3) {
+				(+this.Arrlast(this.mang)*4+3 == this.demnhom) && this.mang.push(+this.mang[this.mang.length-1]+1);
+			}
+			this.content.scrollToBottom();
+		}
+	}
+	private Arrlast(arr: any): any {
+		return arr[arr.length-1];
+	}
 	private createToast(message: string, toastCtrl: ToastController = this.toastCtrl): void {
 		let toast = toastCtrl.create({
 			message: message,
-			duration: 3000,
+			duration: 5000,
 			position: 'top'});
 		toast.onDidDismiss(() => {
 			console.log('Dismissed toast');
@@ -55,11 +97,12 @@ export class HomePage {
 		toast.present();
 	}
 	private filter(textforEncode: string = "Không tìm thấy từ"): string {
+		var objnga = [["ã","ẵ","ẫ","ẽ","ễ","ĩ","õ","ỗ","ỡ","ũ","ữ","ỹ"],["ả","ẳ","ẩ","ẻ","ể","ỉ","ỏ","ổ","ở","ủ","ử","ỷ"]];
 		var stringFomat = textforEncode.toLowerCase();
-		var rgx = new RegExp('[^a-z0-9áàạảãâấầậẩẫăắằặẳẵéèẹẻẽêếềệểễóòọỏõôốồộổỗơớờợởỡúùụủũưứừựửữíìịỉĩđýỳỵỷỹ.;/-?Ω:=%() ]');
-		var m;
-		while ((m = rgx.exec(stringFomat)) != null) {
-			stringFomat = stringFomat.replace(m[0],'');
+		stringFomat = stringFomat.trim().replace(/\s+/g, ' ');
+		stringFomat = stringFomat.replace(/[^a-z0-9áàạảãâấầậẩẫăắằặẳẵéèẹẻẽêếềệểễóòọỏõôốồộổỗơớờợởỡúùụủũưứừựửữíìịỉĩđýỳỵỷỹ.;/-?Ω:=%() ]/g,'');
+		for(let x in objnga[0]) {
+			stringFomat = stringFomat.replace(new RegExp(objnga[0][x], 'g'), objnga[1][x]);
 		}
 		return stringFomat;
 	}
@@ -70,17 +113,39 @@ export class HomePage {
 		this.setMang();
 		var profileModal = this.modalCtrl.create(Profile, { mang: this.mang,obj: this.obj });
 		profileModal.present();
+		this.createToast('Đã mã '+this.obj.ObjEn.length.toString()+' nhóm điện');
 	}
 	public  changePage(work: boolean): void {
 		(work == true) ? this.kiemtra = true : this.kiemtra = false;
+		this.mang = [0];
+		this.obj = {"ObjDe": [],"ObjEn": []};
+		this.demnhom = 0;
+		this.Textm82 = '';
+	}
+	public  change(way:boolean,a:Number = this.demnhom) :void {
+		this.check = false;
+		this.dem = 0;
+		if(way == true) {
+			if(this.demnhom < this.obj.ObjDe.length && this.obj.ObjDe[this.demnhom.toString()] != 'Error') {
+				this.demnhom = +a+1;
+				console.log('Đếm nhóm : '+this.demnhom.toString());
+				console.log(this.obj.ObjEn[this.demnhom.toString()]);
+				this.search = this.obj.ObjEn[this.demnhom.toString()];
+			}
+		} else {
+			if(this.demnhom > 0) {
+				this.demnhom = +a-1;
+				this.search = this.obj.ObjEn[this.demnhom.toString()];
+	 		}
+		}
 	}
 	private thuchanh(hp: this): object {
 		var TachNhom = function(text1: string,text2: string,kt: number): void {
 			console.log('Tách nhóm '+text1);
 			kt = (text1 == text2) ? 1 : kt+1;
 			hp.thongbao = text1;
-			var tmp = hp.find(hp.db,text1);
-			if(tmp != false) {
+			var tmp = hp.find(hp.db,text1,'chu');
+			if(tmp['so'] != 'Error') {
 				ObjEn[demnhom] = tmp['so'];
 				ObjDe[demnhom] = text1;
 				demnhom += 1;
@@ -101,15 +166,17 @@ export class HomePage {
 		console.log("thuchanh ok");
 		return returnobj;
 	}
-	public  find(objects: any,tukhoa: string): any {
-		var dem = objects.chu.length;
+	public  find(objects: any,tukhoa: string,field: string): any {
+		var dem = objects[field].length;
 		var i;
+		var objreturn;
 		for(i=0; i<dem; i++) {
-			if(objects.chu[i] == tukhoa) {
-				return {so:objects.so[i],chu:objects.chu[i]}
+			if(objects[field][i] == tukhoa) {
+				return {so:objects['so'][i],chu:objects['chu'][i]}
 			}
 		}
-		return false;
+		objreturn = (field == 'so') ? {chu:'Error'} : {so:'Error'};
+		return objreturn;
 	}
 	private setMang(): void {
 		var dkien = Math.ceil(this.obj.demnhom/4);
